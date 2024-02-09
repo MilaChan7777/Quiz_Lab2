6b0fd63923f797728c5888bccb5f717dffb6e07e
MilaChan7777 <camila.lerma1@u.icesi.edu.co>
2024-02-09 17:57:53 -0500

    Quiz cambio 2

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const studentGrades = [100, 50, 65, 70, 85, 96, 45];
-const result = computeGrades(studentGrades);
-console.log(result);
+//const studentGrades = [100, 50, 65, 70, 85, 96, 45];
+//const result = computeGrades(studentGrades);
+//console.log(result);
 
-studentGrades.forEach((grades) => {
-	console.log(result);
+function computeGrades(studentGrades) {
+	let gradesInfo = {
+		parsed: 0,
+		passed: 0,
+		failed: 0,
+	};
+}
+
+studentGrades.forEach((grade) => {
+	if (grade >= 90) {
+		if (grade <= 100) {
+			gradesInfo.parsed.push('A');
+		}
+	}
 });
